bf586eca7ccce5550bd28a34e59ae60c0bdd41ce
William Plunkett Jr <joey.plunkett92@yahoo.com>
2018-07-12 04:24:25 -0500

    Update Custom_Vehicles.lua

--- Custom_Vehicles.lua
+++ Custom_Vehicles.lua
@@ -1,12 +1,12 @@
 # Custom Civilian Vehicles
-- megaram             -- Dodge Ram Monster								-- Off-Road			-- $
-- rancherxlextreme    -- Rancher XL Monster								-- Off-Road			-- $
-- sandkinghd          -- Sandking HD Monster							-- Off-Road			-- $
-- vapidlow            -- Sandking Lowrider								-- Muscle			-- $
-- caracarase          -- CaraCara Special Edition						-- Off-Road			-- 
-- huraperfospy        -- 2019 Lamborghini Huracan Performance Spyder	-- Super			-- $
-- vantage             -- 2019 Aston Martin Vantage						-- Sports			-- $
-- camry18             -- 2018 Toyota Camry XSE							-- Sedan			-- $
+- megaram             -- Dodge Ram Monster                            -- Off-Road     -- $
+- rancherxlextreme    -- Rancher XL Monster                           -- Off-Road     -- $
+- sandkinghd          -- Sandking HD Monster                          -- Off-Road     -- $
+- vapidlow            -- Sandking Lowrider                            -- Muscle       -- $
+- caracarase          -- CaraCara Special Edition                     -- Off-Road     -- $ 
+- huraperfospy        -- 2019 Lamborghini Huracan Performance Spyder  -- Super        -- $
+- vantage             -- 2019 Aston Martin Vantage                    -- Sports			-- $
+- camry18             -- 2018 Toyota Camry XSE                        -- Sedan			-- $
 - fpino               -- 2018 Ferrari Portofino							-- Super			-- $
 - malibu              -- 2018 Chevrolet Malibu							-- Sedan			-- $
 - raptor2017          -- 2017 Ford Raptor								-- Off-Road			-- $
