375c194f7b3f08c9819010dccb39858e90f77dc6
William Plunkett Jr <joey.plunkett92@yahoo.com>
2018-07-22 01:37:00 -0500

    Update Custom_Vehicles.lua

--- Custom_Vehicles.lua
+++ Custom_Vehicles.lua
@@ -1,18 +1,63 @@
 # Custom Civilian Vehicles
-- megaram				-- Dodge Ram Monster							-- Off-Road			-- $
-- rancherxlextreme		-- Rancher XL Monster							-- Off-Road			-- $
-- sandkinghd			-- Sandking HD Monster							-- Off-Road			-- $
-- vapidlow				-- Sandking Lowrider							-- Muscle			-- $
-- caracarase			-- CaraCara Special Edition						-- Off-Road			-- $
-- huraperfospy			-- 2019 Lamborghini Huracan Performance Spyder	-- Super			-- $
-- vantage				-- 2019 Aston Martin Vantage					-- Sports			-- $
-- camry18				-- 2018 Toyota Camry XSE						-- Sedan			-- $
-- fpino					-- 2018 Ferrari Portofino						-- Super			-- $
-- malibu				-- 2018 Chevrolet Malibu						-- Sedan			-- $
-- raptor2017			-- 2017 Ford Raptor								-- Off-Road			-- $
-- cczl					-- 2017 Chevy Camaro ZL1						-- Sports			-- $
-- mlnovitec				-- 2017 Maserati Levante Novitec				-- SUV				-- $
-- gtr					-- 2017 Nissan GTR								-- Sports			-- $
-- urus					-- 2016 Bentley Bentayga						-- SUV				-- $
-- centenario			-- 2016 Lamborghini Centenario LP770			-- Super			-- $
-
+- megaram             -- Dodge Ram Monster                              -- Off-Road         -- $
+- rancherxlextreme    -- Rancher XL Monster                             -- Off-Road         -- $
+- sandkinghd          -- Sandking HD Monster                            -- Off-Road         -- $
+- vapidlow            -- Sandking Lowrider                              -- Muscle           -- $
+- caracarase          -- CaraCara Special Edition                       -- Off-Road         -- $
+- huraperfospy        -- 2019 Lamborghini Huracan Performance Spyder    -- Super            -- $
+- vantage             -- 2019 Aston Martin Vantage                      -- Sports           -- $
+- camry18             -- 2018 Toyota Camry XSE                          -- Sedan            -- $
+- fpino               -- 2018 Ferrari Portofino                         -- Super            -- $
+- malibu              -- 2018 Chevrolet Malibu                          -- Sedan            -- $
+- raptor2017          -- 2017 Ford Raptor                               -- Off-Road         -- $
+- cczl                -- 2017 Chevy Camaro ZL1                          -- Sports           -- $
+- mlnovitec           -- 2017 Maserati Levante Novitec                  -- SUV              -- $
+- gtr                 -- 2017 Nissan GTR                                -- Sports           -- $
+- urus                -- 2016 Bentley Bentayga                          -- SUV              -- $
+- centenario          -- 2016 Lamborghini Centenario LP770              -- Super            -- $
+- tahoe               -- 2016 Chevrolet Tahoe                           -- SUV              -- $
+- 16charger           -- 2016 Dodge Hellcat                             -- Sports           -- $
+- zentenario          -- 2016 Lamborghini Centenario Widebody           -- Super            -- $
+- r8v10abt            -- 2016 Audi r8 v10 ABT                           -- Super            -- $
+- ageraone            -- 2015 Koenigsegg Agera One                      -- Super            -- $
+- aventador           -- 2015 Lamborghini Aventador LP700               -- Super            -- $
+- rmodlp750           -- 2015 Lamborghini Aventador LP750               -- Super            -- $
+- laferrari           -- 2015 Ferrari Laferrari                         -- Super            -- $
+- mustangbkit         -- 2015 Ford Mustang Widebody                     -- Muscle           -- $
+- rmz250              -- 2015 Suzuki RMZ250                             -- Motorcycles      -- $
+- mc250               -- 2014 Mercedes-Benz C250                        -- Sports           -- $
+- bmws                -- 2014 BMW S1000 RR                              -- Motorcycles      -- $
+- p1                  -- 2014 McLaren P1                                -- Super            -- $
+- c7                  -- 2014 Chevrolet Corvette C7 Stingray            -- Super            -- $
+- gts8                -- 2014 Henry Clay Veloce GTS-8                   -- Sport Classic    -- $
+- mst                 -- 2013 Ford Mustang GT500                        -- Super            -- $
+- pct13               -- 2013 Porsche Cayenne Turbo                     -- SUV              -- $
+- evoq                -- 2012 Range Rover Evoque                        -- SUV              -- $
+- lumma750            -- 2011 BMW Lumma CLR 750                         -- Sedan            -- $
+- f458                -- 2010 Ferrari 458 Italia                        -- Super            -- $
+- moss                -- 2010 Mercedes-Benz SLR Stirling Moss           -- Super            -- $
+- spyker              -- 2009 Spyker C8 Aileron                         -- Super            -- $
+- rrs08               -- 2008 Range Rover Supercharged                  -- SUV              -- $
+- benzsl63            -- 2008 Mercedes-Benz SL 63 AMG                   -- Super            -- $
+- evo9                -- 2006 Mitsubishi Lancer Evolution IX MR         -- Sports           -- $
+- h6                  -- 2005 Hummer H6                                 -- SUV              -- $
+- skyline             -- 2002 Nissan Skyline GT-R                       -- Sports           -- $
+- civik               -- 2000 Honda Civic Coupe                         -- Sedan            -- $
+- saleen              -- 2000 Ford Mustang Saleen                       -- Super            -- $
+- civic               -- 1999 Honda Civic Si                            -- Sedan            -- $
+- supra2              -- 1998 Toyota Supra                              -- Sports           -- $
+- rmodm3e36           -- 1997 BMW M3 E36 V8                             -- Sports           -- $
+- f1                  -- 1993 McLaren F1                                -- Super            -- $
+- m5e34               -- 1991 BMW E34 M5                                -- Sedan            -- $
+- silv86              -- 1986 Chevrolet Silverado                       -- SUV              -- $
+- cherokee1           -- 1984 Jeep Cherokee Sport Off Road 4x4          -- Off-Road         -- $
+- f288gto             -- 1984 Ferrari 288 GTO                           -- Sport Classic    -- $
+- gtx                 -- 1971 Plymouth RoadRunner GTX 440               -- Muscle           -- $
+- chall70             -- 1970 Dodge Challenger R/T Hemi                 -- Muscle           -- $
+- chall70drag         -- 1970 Dodge Challenger R/T Hemi Drag            -- Muscle           -- $
+- chevelle1970        -- 1970 Chevrolet Chevelle SS                     -- Muscle           -- $
+- firebird            -- 1970 Pontiac Firebird                          -- Muscle           -- $
+- camaro_ss           -- 1969 Chevrolet Camaro SS                       -- Muscle           -- $
+- charger             -- 1969 Dodge Charger R/T                         -- Muscle           -- $
+- c10custom           -- 1966 Chevrolet C-10 Stepside                   -- Muscle           -- $
+- stingray66          -- 1966 Chevrolet Corvette Stingray               -- Sport Classic    -- $
