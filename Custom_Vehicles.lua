59e7fc974536819096ddfdbb260f9ab14a27939a
William Plunkett Jr <joey.plunkett92@yahoo.com>
2018-11-02 16:23:34 -0500

    New Map
    Added New Map Files & Fixed Vehicle Prices.

--- Custom_Vehicles.lua
+++ Custom_Vehicles.lua
@@ -4,66 +4,70 @@
 - rancherxlextreme    -- Rancher XL Monster                             -- Off-Road         -- $100,000         -- 
 - sandkinghd          -- Sandking HD Monster                            -- Off-Road         -- $100,000         -- 
 - vapidlow            -- Sandking Lowrider                              -- Muscle           -- $45,000          -- 
+- ysfbikea            -- Sci Fi Hover Bike                              -- Motorcycles      -- $15,000,000      -- 
 - caracarase          -- CaraCara Special Edition                       -- Off-Road         -- $100,000         -- 
-- huraperfospy        -- 2019 Lambo Huracan Performance Spyder          -- Super            -- $2,500,000       -- 
-- vantage             -- 2019 Aston Martin Vantage                      -- Sports           -- $125,000         -- 
-- camry18             -- 2018 Toyota Camry XSE                          -- Sedan            -- $75,000          -- 
-- fpino               -- 2018 Ferrari Portofino                         -- Super            -- $3,200,000       -- 
-- malibu              -- 2018 Chevy Malibu                              -- Sedan            -- $125,000         -- 
-- raptor2017          -- 2017 Ford Raptor                               -- Off-Road         -- $95,000          -- 
-- cczl                -- 2017 Chevy Camaro ZL1                          -- Sports           -- $250,000         -- 
-- mlnovitec           -- 2017 Maserati Levante Novitec                  -- SUV              -- $95,000          -- 
-- gtr                 -- 2017 Nissan GTR                                -- Sports           -- $300,000         -- 
-- urus                -- 2016 Bentley Bentayga                          -- SUV              -- $50,000          -- 
-- centenario          -- 2016 Lambo Centenario LP770                    -- Super            -- $2,300,000       -- 
-- tahoe               -- 2016 Chevy Tahoe                               -- SUV              -- $105,000         -- 
-- 16charger           -- 2016 Dodge Hellcat                             -- Sports           -- $600,000         -- 
-- zentenario          -- 2016 Lambo Centenario Widebody                 -- Super            -- $4,000,000       -- 
-- r8v10abt            -- 2016 Audi r8 v10 ABT                           -- Super            -- $2,250,000       -- 
-- ageraone            -- 2015 Koenigsegg Agera One                      -- Super            -- $2,600,000       -- 
-- aventador           -- 2015 Lambo Aventador LP700                     -- Super            -- $2,300,000       -- 
-- rmodlp750           -- 2015 Lambo Aventador LP750                     -- Super            -- $2,300,000       -- 
-- laferrari           -- 2015 Ferrari Laferrari                         -- Super            -- $2,000,000       -- 
-- mustangbkit         -- 2015 Ford Mustang Widebody                     -- Muscle           -- $56,000          -- 
+- huraperfospy        -- 2019 Lambo Huracan Performance Spyder          -- Super            -- $1,200,000       -- 
+- vantage             -- 2019 Aston Martin Vantage                      -- Sports           -- $150,000         -- 
+- camry18             -- 2018 Toyota Camry XSE                          -- Sedan            -- $80,000          -- 
+- fpino               -- 2018 Ferrari Portofino                         -- Super            -- $1,200,000       -- 
+- malibu              -- 2018 Chevy Malibu                              -- Sedan            -- $80,000          -- 
+- raptor2017          -- 2017 Ford Raptor                               -- Off-Road         -- $100,000         -- 
+- cczl                -- 2017 Chevy Camaro ZL1                          -- Sports           -- $150,000         -- 
+- mlnovitec           -- 2017 Maserati Levante Novitec                  -- SUV              -- $80,000          -- 
+- gtr                 -- 2017 Nissan GTR                                -- Super            -- $1,100,000       -- 
+- urus                -- 2016 Bentley Bentayga                          -- SUV              -- $80,000          -- 
+- centenario          -- 2016 Lambo Centenario LP770                    -- Super            -- $1,200,000       -- 
+- tahoe               -- 2016 Chevy Tahoe                               -- SUV              -- $80,000          -- 
+- 16charger           -- 2016 Dodge Hellcat                             -- Sports           -- $150,000         -- 
+- zentenario          -- 2016 Lambo Centenario Widebody                 -- Super            -- $1,200,000       -- 
+- r8v10abt            -- 2016 Audi r8 v10 ABT                           -- Super            -- $1,200,000       -- 
+- ageraone            -- 2015 Koenigsegg Agera One                      -- Super            -- $1,200,000       -- 
+- aventador           -- 2015 Lambo Aventador LP700                     -- Super            -- $1,200,000       -- 
+- rmodlp750           -- 2015 Lambo Aventador LP750                     -- Super            -- $1,200,000       -- 
+- laferrari           -- 2015 Ferrari Laferrari                         -- Super            -- $1,200,000       -- 
+- mustangbkit         -- 2015 Ford Mustang Widebody                     -- Muscle           -- $68,000          -- 
 - rmz250              -- 2015 Suzuki RMZ250                             -- Motorcycles      -- $8,000           -- 
-- mc250               -- 2014 Mercedes-Benz C250                        -- Sports           -- $145,000         -- 
+- mc250               -- 2014 Mercedes-Benz C250                        -- Sports           -- $150,000         -- 
 - bmws                -- 2014 BMW S1000 RR                              -- Motorcycles      -- $25,000          -- 
-- p1                  -- 2014 McLaren P1                                -- Super            -- $2,250,000       -- 
-- c7                  -- 2014 Chevy Corvette C7 Stingray                -- Super            -- $2,150,000       -- 
-- gts8                -- 2014 Henry Clay Veloce GTS-8                   -- Sport Classic    -- $2,400,000       -- 
-- mst                 -- 2013 Ford Mustang GT500                        -- Super            -- $2,300,000       -- 
-- pct13               -- 2013 Porsche Cayenne Turbo                     -- SUV              -- $125,000         -- 
-- evoq                -- 2012 Range Rover Evoque                        -- SUV              -- $35,000          -- 
-- lumma750            -- 2011 BMW Lumma CLR 750                         -- Sedan            -- $55,000          -- 
-- f458                -- 2010 Ferrari 458 Italia                        -- Super            -- $3,000,000       -- 
-- moss                -- 2010 Mercedes-Benz SLR Stirling Moss           -- Super            -- $2,550,000       -- 
-- spyker              -- 2009 Spyker C8 Aileron                         -- Super            -- $2,500,000       -- 
-- rrs08               -- 2008 Range Rover Supercharged                  -- SUV              -- $45,000          -- 
-- benzsl63            -- 2008 Mercedes-Benz SL 63 AMG                   -- Super            -- $2,200,000       -- 
-- evo9                -- 2006 Mitsubishi Lancer Evolution IX MR         -- Sports           -- $100,000         -- 
+- p1                  -- 2014 McLaren P1                                -- Super            -- $1,200,000       -- 
+- c7                  -- 2014 Chevy Corvette C7 Stingray                -- Super            -- $1,100,000       -- 
+- gts8                -- 2014 Henry Clay Veloce GTS-8                   -- Sport Classic    -- $85,000          -- 
+- mst                 -- 2013 Ford Mustang GT500                        -- Super            -- $1,100,000       -- 
+- pct13               -- 2013 Porsche Cayenne Turbo                     -- SUV              -- $80,000          -- 
+- evoq                -- 2012 Range Rover Evoque                        -- SUV              -- $80,000          -- 
+- lumma750            -- 2011 BMW Lumma CLR 750                         -- Sedan            -- $80,000          -- 
+- f458                -- 2010 Ferrari 458 Italia                        -- Super            -- $1,200,000       -- 
+- moss                -- 2010 Mercedes-Benz SLR Stirling Moss           -- Super            -- $1,100,000       -- 
+- spyker              -- 2009 Spyker C8 Aileron                         -- Super            -- $1,100,000       -- 
+- rrs08               -- 2008 Range Rover Supercharged                  -- SUV              -- $80,000          -- 
+- benzsl63            -- 2008 Mercedes-Benz SL 63 AMG                   -- Super            -- $1,100,000       -- 
+- evo9                -- 2006 Mitsubishi Lancer Evolution IX MR         -- Sports           -- $150,000         -- 
 - h14x4               -- 2006 Hummer H1 4x4                             -- Off-Road         -- $100,000         --
 - h16x6               -- 2006 Hummer H1 6x6                             -- Off-Road         -- $100,000         --
-- h6                  -- 2005 Hummer H6                                 -- SUV              -- $30,500          -- 
-- skyline             -- 2002 Nissan Skyline GT-R                       -- Sports           -- $175,000         -- 
-- civik               -- 2000 Honda Civic Coupe                         -- Sedan            -- $65,000          -- 
-- saleen              -- 2000 Ford Mustang Saleen                       -- Super            -- $2,800,000       -- 
-- civic               -- 1999 Honda Civic Si                            -- Sedan            -- $56,000          -- 
-- supra2              -- 1998 Toyota Supra                              -- Sports           -- $215,000         -- 
-- rmodm3e36           -- 1997 BMW M3 E36 V8                             -- Sports           -- $195,000         -- 
-- f1                  -- 1993 McLaren F1                                -- Super            -- $2,100,000       -- 
-- m5e34               -- 1991 BMW E34 M5                                -- Sedan            -- $57,000          -- 
-- silv86              -- 1986 Chevy Silverado                           -- SUV              -- $25,000          -- 
-- cherokee1           -- 1984 Jeep Cherokee Sport Off Road 4x4          -- Off-Road         -- $95,000          -- 
-- f288gto             -- 1984 Ferrari 288 GTO                           -- Sport Classic    -- $2,400,000       -- 
-- gtx                 -- 1971 Plymouth RoadRunner GTX 440               -- Muscle           -- $57,000          -- 
-- chall70             -- 1970 Dodge Challenger R/T Hemi                 -- Muscle           -- $225,000         -- 
-- chall70drag         -- 1970 Dodge Challenger R/T Hemi Drag            -- Muscle           -- $5,000,000       -- 
+- h6                  -- 2005 Hummer H6                                 -- SUV              -- $70,000          -- 
+- skyline             -- 2002 Nissan Skyline GT-R                       -- Sports           -- $150,000         -- 
+- civik               -- 2000 Honda Civic Coupe                         -- Sedan            -- $80,000          -- 
+- saleen              -- 2000 Ford Mustang Saleen                       -- Super            -- $1,100,000       -- 
+- civic               -- 1999 Honda Civic Si                            -- Sedan            -- $80,000          -- 
+- supra2              -- 1998 Toyota Supra                              -- Sports           -- $150,000         -- 
+- rmodm3e36           -- 1997 BMW M3 E36 V8                             -- Sports           -- $150,000         -- 
+- f1                  -- 1993 McLaren F1                                -- Super            -- $1,100,000       -- 
+- m5e34               -- 1991 BMW E34 M5                                -- Sedan            -- $80,000          -- 
+- silv86              -- 1986 Chevy Silverado                           -- SUV              -- $70,000          -- 
+- cherokee1           -- 1984 Jeep Cherokee Sport Off Road 4x4          -- Off-Road         -- $100,000         -- 
+- f288gto             -- 1984 Ferrari 288 GTO                           -- Sport Classic    -- $85,000          -- 
+- gtx                 -- 1971 Plymouth RoadRunner GTX 440               -- Muscle           -- $70,000          -- 
+- chall70             -- 1970 Dodge Challenger R/T Hemi                 -- Muscle           -- $75,000          -- 
+- chall70drag         -- 1970 Dodge Challenger R/T Hemi Drag            -- Muscle           -- $10,000,000      -- 
 - chevelle1970        -- 1970 Chevy Chevelle SS                         -- Muscle           -- $52,000          -- 
 - firebird            -- 1970 Pontiac Firebird                          -- Muscle           -- $54,000          -- 
 - camaro_ss           -- 1969 Chevy Camaro SS                           -- Muscle           -- $52,000          -- 
 - charger             -- 1969 Dodge Charger R/T                         -- Muscle           -- $52,000          -- 
-- c10custom           -- 1966 Chevy C-10 Stepside                       -- Muscle           -- $50,000          -- 
-- stingray66          -- 1966 Chevy Corvette Stingray                   -- Sport Classic    -- $145,000         -- 
+- c10custom           -- 1966 Chevy C-10 Stepside                       -- Muscle           -- $46,000          -- 
+- stingray66          -- 1966 Chevy Corvette Stingray                   -- Sport Classic    -- $85,000          -- 
+
+# Custom Boats -- WIP!!!
+- rboat               -- Rapid Speed Boat                               -- Boats            -- $48,000          -- 
 
 # Custom Jobs Vehicles
 - tow01               -- Scania 113H Tow Truck
@@ -115,10 +119,26 @@
 - cgd01               -- Coast Guard Defender
 - fbi01               -- 2013 Charger
 - fbi02               -- 2013 Tahoe
+- fbi03               -- 2016 Dodge Challenger
 - biker01             -- Western Police Bike
 - biker02             -- BMW Police Bike
 - ptv01               -- Prisoner Transport Van
 - polmav              -- Aviation
 - firehawk            -- S70 Fire Hawk
 - riot01              -- SRT/SWAT Bearcat
-- riot02			        -- SRT/SWAT Rioter
+- riot02			  -- SRT/SWAT Rioter
+
+# Blocked/Replaced Default Cars
+- fbi                 -- 2011 G6 Security CVPI
+- fbi2                -- 2011 G6 Security CVPI
+- police              -- 2011 G6 Security CVPI
+- police2             -- 2011 G6 Security CVPI
+- police3             -- 2011 G6 Security CVPI
+- police4             -- 2011 G6 Security CVPI
+- policeold1          -- 2011 G6 Security CVPI
+- policeold2          -- 2011 G6 Security CVPI
+- pranger             -- 2011 G6 Security CVPI
+- sheriff             -- 2011 G6 Security CVPI
+- sheriff2            -- 2011 G6 Security CVPI
+
+-----------------------------------------------------------------------------------------------------------------------
